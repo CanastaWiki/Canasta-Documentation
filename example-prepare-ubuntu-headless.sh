5eccf10693dfc1e3a7e0e8f95853d7f7e1c417c0
Greg Rundlett <greg@freephile.com>
2023-11-30 11:18:02 -0500

    rename example prepare script; add comments

--- example-prepare-ubuntu-headless.sh
+++ example-prepare-ubuntu-headless.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 set -e
 
-# This script is an EXAMPLE of a script that can prepare your host for Canasta by installing the 
+# This script is an EXAMPLE of a script that can prepare a cloud host for Canasta by installing the 
 # proper Docker software used by the Canasta Stack when Docker Engine is your container engine
-# and Docker Compose is your orchestrator
+# and Docker Compose is your orchestrator, in a 'headless' (server) environment. In other words,
+# This script is useful when you are not working on a local machine where Docker Desktop would be 
+# the preferred way of preparing the host.
 
-# This example is specific to Ubuntu
+# This example is specific to Ubuntu Linux
 
 # This example is to prepare a TEST host. Using it on a live system, production environment or daily driver is 
-# NOT recommended.
+# NOT recommended as it does not make any claims of being complete. For instance, no system hardening is part of 
+# this example.
 
 # Add Docker's official GPG key:
 sudo apt-get update
